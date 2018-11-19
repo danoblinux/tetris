0ad3969b67086da564600d2bb70bbb532284c028
Dano Bubanec <danoblinux@gmail.com>
2018-11-19 13:47:16 +0100

    Prototype

--- js/tetris.js
+++ js/tetris.js
@@ -19,7 +19,7 @@ var camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR );
 var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-
+document.addEventListener("keydown", onDocumentKeyDown, false);
 /*var fillBoard = function () {
     for(var i = 0; i <= CUBE_SIZE * GRID_WIDTH; i+=CUBE_SIZE){
         for(var j = 0; j <= CUBE_SIZE * GRID_HEIGHT; j+=CUBE_SIZE){
@@ -45,6 +45,7 @@ var cube = new THREE.Mesh(
     new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: false } )
 );
 cube.position.y = 120 - BOX_SIZE / 2;
+cube.position.x = 0 - OFFSET;
 scene.add(cube);
 var cube2 = new THREE.Mesh(
     new THREE.BoxGeometry(BOX_SIZE, BOX_SIZE, Z
@@ -52,14 +53,14 @@ var cube2 = new THREE.Mesh(
     new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: false } )
 );
 cube2.position.y = 120 - BOX_SIZE / 2;
-cube2.position.x = 10;
+cube2.position.x = 10 - OFFSET;
 var cube3 = new THREE.Mesh(
     new THREE.BoxGeometry(BOX_SIZE, BOX_SIZE, Z
     ),
     new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: false } )
 );
 cube3.position.y = 120 - BOX_SIZE / 2;
-cube3.position.x = 20;
+cube3.position.x = 20 - OFFSET;
 scene.add(cube2);
 scene.add(cube3);
 camera.position.z = 180;
@@ -83,6 +84,21 @@ var animate = function () {
 };
 
 animate();
+
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == 37 && cube.position.x >= -50 + BOX_SIZE){
+        cube.position.x -= BOX_SIZE;
+        cube2.position.x -= BOX_SIZE;
+        cube3.position.x -= BOX_SIZE;
+    }else if (keyCode == 39 && cube3.position.x <= 50 - BOX_SIZE){
+        cube.position.x += BOX_SIZE;
+        cube2.position.x += BOX_SIZE;
+        cube3.position.x += BOX_SIZE;
+    }else if (keyCode == 40 && cube3.position.x <= 50 - BOX_SIZE){
+        SPEED = 3;
+    }
+}
 
 if ( !window.requestAnimationFrame ) {
     window.requestAnimationFrame = ( function() {
